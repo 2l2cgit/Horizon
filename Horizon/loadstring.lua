89f243d1023722ae4b964f22d85d3311a982b53c
2l2cgit <97702057+2l2cgit@users.noreply.github.com>
2022-06-17 20:59:37 +0200

    Update loadstring.lua

--- Horizon/loadstring.lua
+++ Horizon/loadstring.lua
@@ -4,13 +4,13 @@ local betterisfile = function(file)
 end
 
 if not shared.FutureDeveloper and betterisfile("Future/scripts/6872274481.lua") then
-    warn("[Future] Please contact engo#0320 on discord, Error: NVLN Detection!")
+    warn("[Bruh] Please contact engo#0320 on discord, Error: NVLN Detection!")
     return pcall(game.Players.LocalPlayer.Kick, game.Players.LocalPlayer, "Please contact engo#0320 on discord, Error: NVLN Detection!")
 end
 
 if shared.FutureDeveloper then 
-    loadfile("Future/Initiate.lua")()
+    loadfile("Horizon/Initiate.lua")()
     return
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/Future/main/Initiate.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2l2cgit/Horizon/main/Initiate.lua"))()
